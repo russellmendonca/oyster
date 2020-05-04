0bdc46e650e47615a459d7eaad53ead735907c05
russell <russellm@berkeley.edu>
2020-05-04 20:29:59 +0000

    latest

--- pearl-train-launcher.sh
+++ pearl-train-launcher.sh
@@ -3,9 +3,9 @@ export N_GPUS=$(nvidia-smi -L | wc -l)
 export N_JOBS=48
 
 parallel -j $N_JOBS \
-    'CUDA_VISIBLE_DEVICES=$(({%} % $N_GPUS))' python launch_pearl.py ./configs/ant-legs-var-length.json \
-     --log_annotation default \
+    'CUDA_VISIBLE_DEVICES=$(({%} % $N_GPUS))' python launch_pearl.py ./configs/cheetah-vel.json \
+     --log_annotation restricted_train_set \
      --seed={1} \
      --num_train_steps_per_itr={2} \
     ::: 0 1 \
-    ::: 1000 2000 4000
+    ::: 1000 2000
